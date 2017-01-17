8d6dce4f144060129d2514e9ccd4e67269f6090e
janzolau1987 <janzolau1987@gmail.com>
2017-01-17 21:30:40 +0800

    yaoyaohao.com网站商品爬取

---
+++ yyh/yyh/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class YyhItem(scrapy.Item):
+    # define the fields for your item here like:
+    goods_id = scrapy.Field()
+    goods_name = scrapy.Field()
+    manufacture = scrapy.Field()
---
+++ yyh/yyh/spiders/yyh_spider.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+from yyh.items import YyhItem
+
+class YyhSpider(scrapy.Spider):
+    name = "yaoyaohao.com"
+    allowed_domains = ["yaoyaohao.com"]
+    start_urls = [
+        "http://www.yaoyaohao.com"
+    ]
+
+    def parse(self, response):
+        for sel in response.xpath('//ul[@class="sort-list"]/li'):
+            next_page = sel.xpath('h4/a/@href').extract_first()
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parse_search)
+
+    def parse_search(self, response):
+        for sel in response.xpath('//ul[@id="searchTableBody"]/li'):
+            item = YyhItem()
+            item['goods_id'] = sel.xpath('@id').extract_first()
+            pinfo = sel.xpath('div[@class="p-desc"]/pinfo/p').extract()
+            item['goods_name'] = pinfo[0]
+            item['manufacture'] = pinfo[1]
+            yield item
